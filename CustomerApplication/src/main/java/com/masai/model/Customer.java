76371ccd4d3f387af601d183a777bc6972e88d0f
Mayank Dagar <105916896+MayankDagar77@users.noreply.github.com>
2023-06-09 23:17:09 +0530

    Add files via upload

--- CustomerApplication/src/main/java/com/masai/model/Customer.java
+++ CustomerApplication/src/main/java/com/masai/model/Customer.java
@@ -4,6 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Pattern;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -18,8 +20,13 @@ public class Customer {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer cid;
+	
 	private String name;
+
+	@Email(message = "Enter a valid email address")
 	private String email;
+	
+	@Pattern(regexp = "\\d{10}", message = "Enter a 10-digit phone number")
 	private String phoneNumber;
 	
 	
